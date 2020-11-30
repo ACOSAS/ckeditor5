45814b532ecf7ae794c99c2b868effb0da8dca9e
Christian Myksvoll <chrimyk@acos.no>
2020-11-30 11:51:54 +0100

    Standard text plugin styling

--- packages/ckeditor5-build-classic/src/plugins/standardtekster/standardtekster.js
+++ packages/ckeditor5-build-classic/src/plugins/standardtekster/standardtekster.js
@@ -7,6 +7,7 @@ import { createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
 
 import textIcon from '../standardtekster/icons/text.svg';
 import folderIcon from '../standardtekster/icons/folder.svg';
+import folderOpenIcon from '../standardtekster/icons/folder-open.svg';
 
 import View from '@ckeditor/ckeditor5-ui/src/view';
 
@@ -24,10 +25,7 @@ export default class Standardtekster extends Plugin {
         // Register UI component
         editor.ui.componentFactory.add('standardTekster', locale => {
 
-			//const command = editor.commands.get( 'insertTable' );
 			const dropdownView = createDropdown( locale );
-
-			//dropdownView.bind( 'isEnabled' ).to( command );
 
 			// Decorate dropdown's button.
 			dropdownView.buttonView.set( {
@@ -43,11 +41,18 @@ export default class Standardtekster extends Plugin {
 					return;
 				}
 
-				function toggleFolder(event) {
-					console.log('toggleFolder', event.target);
-					event.target.classList.toggle('open');
+				function toggleFolder(event) {                   
+                    var icons = event.currentTarget.getElementsByClassName('folder-icon');
 
-					var uls = event.target.getElementsByTagName('ul');
+                    for (var i=0; i < icons.length; i++) {
+                        var icon = icons[i];
+
+                        if (icon.parentNode.parentNode == event.currentTarget) {
+                            icon.classList.toggle('hidden');
+                        }                            
+                    }
+
+					var uls = event.currentTarget.getElementsByTagName('ul');
 
 					if (uls.length > 0) {
 						uls[0].classList.toggle('hidden');
@@ -60,11 +65,8 @@ export default class Standardtekster extends Plugin {
 					console.log('Text clicked: ' + this.innerText);
 
 					var content = this.getAttribute("data-content");
-					console.log('content: ' + content);
-
 					const viewFragment = editor.data.processor.toView( content );
 					const modelFragment = editor.data.toModel( viewFragment );
-
 					editor.model.insertContent( modelFragment );
 
 					dropdownView.isOpen = false;
@@ -73,8 +75,6 @@ export default class Standardtekster extends Plugin {
 
 					event.cancelBubble = true;
 				}
-
-                console.log("plugin editor", editor);
 
 				stdTextView = new StandardteksterView(locale, Standardtekster.standardtekstMapper);
 				dropdownView.panelView.children.add( stdTextView );
@@ -104,35 +104,45 @@ class StandardteksterView extends View {
 
 		console.log('StdTextView', mapper);
 
-		var styleContent = '<style>\
-		li > ul {\
-			padding-left: 20px !important;\
-		}\
-		li {\
-			overflow: hidden !important;\
-			padding: 2px 5px 2px 5px !important;\
-		}\
-		.standardtext {\
-			font-style: normal;\
-			font-weight: normal;\
-		}\
-		li.standardtekst:hover {\
-			background: #E0E0FF !important;\
-		}\
-		.hidden {\
-			display: none;\
-		}\
-		.open {\
-			font-style: italic !important;\
-			font-weight: bold !important;\
-		}\
-		.dropdown-container {\
-			padding: 5px !important;\
-		}\
-		</style>';
+		var styleContent = `<style>
+		li > ul {
+			padding-left: 20px !important;
+		}
+		li {
+			overflow: hidden !important;
+		}
+		.actionBtn {
+			padding: 5px 10px 5px 10px !important;
+			display: block !important;
+		}
+		.actionBtn:hover {
+			background: #E0E0FF !important;
+			cursor: pointer !important;
+		}
+		.standardtext {
+			
+		}
+		.hidden {
+			display: none;
+		}
+		.dropdown-container {
+			padding: 5px !important;
+		}
+		</style>`;
+
+		function escapeHtml(str) {
+			const node = document.createElement('span');
+			node.textContent = str;
+			return node.innerHTML;
+		}
 		
 		function lagMappeHtml(mappen) {
-			var tmp = '<li class="folder closed">' + folderIcon + mappen.navn;
+			var tmp = '<li class="folder closed">' + 
+				'<span class="actionBtn">' +
+                '<span class="folder-icon">' + folderIcon + '</span>' + 
+                '<span class="folder-icon hidden">' + folderOpenIcon + '</span>' + 
+				'<span>' + escapeHtml(mappen.navn) + '</span>' +
+				'</span>';
 			
 			if (mappen.mapper.length > 0 || mappen.standardTekster.length > 0)
 				tmp += '<ul class="folder-content hidden">';
@@ -153,7 +163,13 @@ class StandardteksterView extends View {
 		}
 
 		function lagTekstHtml(tekst) {
-			return '<li data-content="' + tekst.innhold + '" class="standardtext">' + textIcon + tekst.navn + '</li>';
+			var tmp = '<li data-content="' + escapeHtml(tekst.innhold) + '" class="standardtext">' +
+				'<span class="actionBtn">' + textIcon + 
+				'<span>' + escapeHtml(tekst.navn) + '</span>' +
+				'</span>' +
+				'</li>'
+
+			return tmp;
 		}
 
 		var htmlContent = '<ul>';
