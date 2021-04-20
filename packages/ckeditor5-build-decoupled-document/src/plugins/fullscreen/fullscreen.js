ae49a07a1ece9d5bb0c0be894936cca6ee31083d
Vasilenko, Eugene <eugene.vasilenko@gmail.com>
2021-04-20 17:29:11 +0300

    add fullscreen plugin

--- packages/ckeditor5-build-decoupled-document/src/plugins/fullscreen/fullscreen.js
+++ packages/ckeditor5-build-decoupled-document/src/plugins/fullscreen/fullscreen.js
@@ -3,7 +3,9 @@ import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 export default class Fullscreen extends Plugin {
 	init() {
 		this.editor.keystrokes.set("CTRL+SHIFT+F", () => {
-			this.editor.fullscreenHandler();
+			if (this.editor.fullscreenHandler) {
+				this.editor.fullscreenHandler();
+			}
 		});
 	}
 }
